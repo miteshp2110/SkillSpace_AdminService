9c8eaada2ecb4ba6c413ecffa26c8d9e1a561639
miteshp2110 <paliwalmitesh2110@gmail.com>
2024-11-05 15:32:04 +0530

    Added all endoints and services

--- src/main/java/com/skillspace/adminservice/controller/TeacherController.java
+++ src/main/java/com/skillspace/adminservice/controller/TeacherController.java
@@ -5,6 +5,7 @@ import com.skillspace.adminservice.model.Users;
 import com.skillspace.adminservice.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,5 +20,9 @@ public class TeacherController {
     @PostMapping("/getAllTeacher")
     public Map<String, List<Users>> getAllTeacher(){
         return teacherService.getAllTeachers();
+    }
+    @PostMapping("/addTeacher")
+    public Map<String,String> addTeacher(@RequestBody Map<String,String> map){
+        return teacherService.addTeacher(map.get("email"),map.get("password"));
     }
 }
--- src/main/java/com/skillspace/adminservice/repository/ProjectRepository.java
+++ src/main/java/com/skillspace/adminservice/repository/ProjectRepository.java
@@ -1,4 +1,20 @@
 package com.skillspace.adminservice.repository;
 
-public interface ProjectRepository {
+
+import com.skillspace.adminservice.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Integer> {
+
+    @Query(value = "select count(*) from project",nativeQuery = true)
+    public int totalProject();
+
+    @Query(value = "select count(*) from project where status_level=4",nativeQuery = true)
+    public int completedProject();
+
+    @Query(value = "select count(*) from project where status_level!=4",nativeQuery = true)
+    public int ongoingProject();
 }
